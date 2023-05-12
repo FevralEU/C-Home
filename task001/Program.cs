916ff8da477cb94c5d9d03968cc40ae434a4fd33
Daniil <Fevral_yt@mail.ru>
2023-05-12 21:59:13 +0300

    Задания

---
+++ task001/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+Console.WriteLine("Введите целое число A");
+int numberOne = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите целое число B");
+int numberTwo = Convert.ToInt32(Console.ReadLine());
+if (numberOne > numberTwo)
+{
+    Console.WriteLine($"{numberOne}>{numberTwo}");
+}
+else 
+{
+    Console.WriteLine($"{numberTwo}>{numberOne}");
+}
---
+++ task002/Program.cs
@@ -0,0 +1,24 @@
+﻿//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.WriteLine("введите колличество чисел");
+int massivecount = Convert.ToInt32(Console.ReadLine());
+int[] Array = new int[massivecount];
+int i=0;
+for (i=0 ;i < Array.Length; i = i + 1)
+{
+    Console.WriteLine($"Введите число под номером {i}");
+    Array[i] = Convert.ToInt32(Console.ReadLine());
+}
+int max = Array[0];
+int b=0;
+for (b=0 ;b < Array.Length; b = b + 1)
+{
+    if (Array[b] > max)
+    {
+        max=Array[b];
+    }
+    else
+    {
+        
+    }
+}
+Console.WriteLine($"Максмальное число {max}");
