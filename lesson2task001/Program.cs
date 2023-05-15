c362dbef9114659f6c77de03c1b69538df5a343e
Daniil <Fevral_yt@mail.ru>
2023-05-15 07:40:54 +0300

    А первое задание коммитнуть?

--- lesson2task001/Program.cs
+++ lesson2task001/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿int Read(string message)
+{
+    System.Console.Write(message);
+    string value = Console.ReadLine();
+    int readedNumber = Convert.ToInt32(value);
+    return readedNumber;
+}
+
+int number = Read("Введите трехзначное число: ");
+if (number < 100 || number >= 1000)
+{
+   Console.WriteLine("Никаких шуток над программой, попробуйте снова)");
+   return;
+}
+int secondRank = number / 10 % 10;
+Console.WriteLine($"Вторая цифра вашего числа: {secondRank}");
