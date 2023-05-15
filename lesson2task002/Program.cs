ab7e361fcab0397d6fa5b45572f318c6ab273d1b
Daniil <Fevral_yt@mail.ru>
2023-05-15 07:25:08 +0300

    Второе Задание

--- lesson2task002/Program.cs
+++ lesson2task002/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int Read(string message)
+{
+    System.Console.Write(message);
+    string value = Console.ReadLine();
+    int readedNumber = Convert.ToInt32(value);
+    return readedNumber;
+}
+int ThirdRank(int number)
+{
+   while (number > 999 || number >= 100)
+   {
+        number = number / 10;
+   }
+   return number % 10;
+}
+bool FalseRank(int number)
+{
+    if (number < 100 );
+    {
+        Console.WriteLine("Число не трехзначное, значит третьей цифры нет");
+        return false;
+    }
+    return true;
+}
+int number = Read("Введите число: ");
+if (FalseRank(number));
+{
+    Console.WriteLine(ThirdRank(number));
+}
