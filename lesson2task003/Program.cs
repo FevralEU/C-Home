618ffc4186e8639b2f36a1238c38241c8082c9c2
Daniil <Fevral_yt@mail.ru>
2023-05-15 07:39:10 +0300

    Задание три

--- lesson2task003/Program.cs
+++ lesson2task003/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int Read(string message)
+{
+    System.Console.Write(message);
+    string value = Console.ReadLine();
+    int readedNumber = Convert.ToInt32(value);
+    return readedNumber;
+}
+bool Weekend(int dayOfWeek)
+{
+    if (dayOfWeek > 5)
+    {
+        return true;
+    }
+    return false;
+}
+bool Weekday(int number)
+{
+    if (number > 0 && number <= 7)
+    {
+        return true;
+    }
+    Console.WriteLine("Вы живете на марсе?");
+    return false;
+}
+int dayOfWeek = Read("Введите номер дня в неделе: ");
+if (Weekday(dayOfWeek))
+{
+    if (Weekend(dayOfWeek))
+    {
+        Console.WriteLine("Выходной, живем, живем");
+    }
+    else
+    {
+        Console.WriteLine("Опять работа?");
+    }
+}
