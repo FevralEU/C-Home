6f485bfee8ffba4e315a97f91a67d38f0f155784
Daniil <Fevral_yt@mail.ru>
2023-05-17 07:30:24 +0300

    задания сделаны

---
+++ lesson3rask003/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите положительное целое число: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+Square(numberN);
+
+void Square(int num)
+{
+int count = 1;
+while (count <= num)
+{
+Console.WriteLine($"{count,3} {count * count * count,5}");
+count++;
+}
+}
