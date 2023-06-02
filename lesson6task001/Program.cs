05e196b91886a9ee1adf78d4bb4ec9bf8991ee29
Daniil <Fevral_yt@mail.ru>
2023-06-02 04:25:06 +0300

    Готово

---
+++ lesson6task001/Program.cs
@@ -0,0 +1,41 @@
+﻿int Prompt (string message)
+{
+System.Console.Write(message);
+string value = Console.ReadLine();
+int result = Convert.ToInt32(value);
+return result;
+}
+int[] InputArray (int length)
+{
+int[] array = new int [length];
+for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Prompt ($"введите {i + 1}-й элeмeнт");
+    }
+return array;
+}
+void PrintArray (int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {   
+        Console.WriteLine($"a[{i}] {array[i]}");
+    }
+}
+int CountPositiveNumbers (int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        }
+    }
+return count;
+}
+int lenght = Prompt("Введите количество элементов >");
+int[] array;
+array = InputArray (lenght);
+
+PrintArray (array);
+Console.WriteLine($"Количество чисел больше 0 - {CountPositiveNumbers (array)}");
